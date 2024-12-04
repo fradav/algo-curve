26069da6d18c24b233e714df4732873794bb2ffe
Guillermo Durand <guillermo.durand@universite-paris-saclay.fr>
2024-12-04 20:19:50 +0100

    allow for multiple settings with a for loop

--- script_test_curve_fast.R
+++ script_test_curve_fast.R
@@ -11,6 +11,8 @@ get_groups <- function(list_groups, leaf_list){
   return(list(res = res, total = unlist(res)))
 }
 
+n_repl <- 10
+
 pow <- 10
 m <- 2 ^ pow
 example <- dyadic.from.height(m, pow, 2)
@@ -23,37 +25,42 @@ mu[H1] <- signal
 
 pval <- 1 - pnorm(mu + rnorm(n = m))
 alpha <- 0.05
-method <- zeta.DKWM
-# method <- zeta.trivial
-ZL <- zetas.tree(C, leaf_list, method, pval, alpha, refine = TRUE, verbose = FALSE)
 
-# print("The pvalues are:")
-# print(pval)
-# print("The zetas are:")
-# print(ZL)
+methods <- c(zeta.DKWM, zeta.trivial)
 
-pruned <- pruning(C, ZL, leaf_list, prune.leafs = FALSE)
-pruned.no.gaps <- pruning(C, ZL, leaf_list, prune.leafs = FALSE, delete.gaps = TRUE)
-
-print(nb.elements(C))
-print(nb.elements(pruned.no.gaps$C))
-
-perm <- 1:m
-
-print("Comparing execution times:")
-mbench <- microbenchmark(naive.not.pruned = curve.V.star.forest.naive(perm, C, ZL, leaf_list),
-                         naive.pruned = curve.V.star.forest.naive(perm, pruned$C, pruned$ZL, leaf_list),
-                         naive.pruned.no.gaps = curve.V.star.forest.naive(perm, pruned.no.gaps$C, pruned.no.gaps$ZL, leaf_list),
-                         fast.not.pruned = curve.V.star.forest.fast(perm, C, ZL, leaf_list),
-                         fast.pruned = curve.V.star.forest.fast(perm, pruned$C, pruned$ZL, leaf_list, is.pruned = TRUE),
-                         fast.pruned.no.gaps = curve.V.star.forest.fast(perm, pruned.no.gaps$C, pruned.no.gaps$ZL, leaf_list, is.pruned = TRUE),
-                         times = 2, check = "equal")
-write.csv(mbench, "benchmark_01.csv", row.names = F)
-loaded <- read.csv("benchmark_01.csv")
-class(loaded) <- c("microbenchmark", class(loaded))
-attr(loaded, "unit") <- "t"
-# print(loaded)
-# boxplot(loaded)
-sum <- summary(loaded, unit="s")
-sum <- sum[c(4, 5, 6, 1, 2, 3), ]
-sum
+for (i in 1:2){
+  
+  method <- methods[i]
+  ZL <- zetas.tree(C, leaf_list, method, pval, alpha, refine = TRUE, verbose = FALSE)
+  
+  # print("The pvalues are:")
+  # print(pval)
+  # print("The zetas are:")
+  # print(ZL)
+  
+  pruned <- pruning(C, ZL, leaf_list, prune.leafs = FALSE)
+  pruned.no.gaps <- pruning(C, ZL, leaf_list, prune.leafs = FALSE, delete.gaps = TRUE)
+  
+  print(nb.elements(C))
+  print(nb.elements(pruned.no.gaps$C))
+  
+  perm <- 1:m
+  
+  print("Comparing execution times:")
+  mbench <- microbenchmark(naive.not.pruned = curve.V.star.forest.naive(perm, C, ZL, leaf_list),
+                           naive.pruned = curve.V.star.forest.naive(perm, pruned$C, pruned$ZL, leaf_list),
+                           naive.pruned.no.gaps = curve.V.star.forest.naive(perm, pruned.no.gaps$C, pruned.no.gaps$ZL, leaf_list),
+                           fast.not.pruned = curve.V.star.forest.fast(perm, C, ZL, leaf_list),
+                           fast.pruned = curve.V.star.forest.fast(perm, pruned$C, pruned$ZL, leaf_list, is.pruned = TRUE),
+                           fast.pruned.no.gaps = curve.V.star.forest.fast(perm, pruned.no.gaps$C, pruned.no.gaps$ZL, leaf_list, is.pruned = TRUE),
+                           times = n_repl, check = "equal")
+  write.csv(mbench, paste0("benchmark_0", i, ".csv"), row.names = F)
+  loaded <- read.csv("benchmark_01.csv")
+  class(loaded) <- c("microbenchmark", class(loaded))
+  attr(loaded, "unit") <- "t"
+  # print(loaded)
+  # boxplot(loaded)
+  sum <- summary(loaded, unit="s")
+  sum <- sum[c(4, 5, 6, 1, 2, 3), ]
+  sum
+}
