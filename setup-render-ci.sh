c8aa320b8ecf85144fc31b48e42aa8790fc7cfc0
François-David Collin <Francois-David.Collin@umontpellier.fr>
2025-02-11 12:10:02 +0100

    sync with the extension

---
+++ setup-render-ci.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y inkscape
+mkdir -p ~/.local/bin
+~/.TinyTeX/bin/x86_64-linux/tlmgr option sys_bin ~/.local/bin
+~/.TinyTeX/bin/x86_64-linux/tlmgr path add
+~/.TinyTeX/bin/x86_64-linux/tlmgr update --self
